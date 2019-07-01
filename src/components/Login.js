a7233cf81a7fc3a5fe415dbd69bb1e1e9375f8f0
girin-dev <kw8799@gmail.com>
2019-07-01 17:29:44 +0900

    [기능추가] Login 컴포넌트 생성

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+          로그인 컴포넌트
+      </div>
+    );
+  }
+}
+
+export default Login;
