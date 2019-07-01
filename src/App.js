8aa5f09d4ee54072f955bd5c051d26b4ecbc1925
girin-dev <kw8799@gmail.com>
2019-07-01 16:34:43 +0900

    [기능추가] Mgamap컴포넌트 - 네이버 지도 및 현위치 설정

--- src/App.js
+++ src/App.js
@@ -5,11 +5,36 @@ import Mgkmap from './components/Mgkmap';
 import Chatlist from './components/Chatlist';
 
 class App extends Component {
+  constructor(props){
+    // 초기화 담당
+    super(props);
+    this.state = {
+      currentCoords:{
+        longitude:0,
+        latitude:0
+      },
+      defaultCoords:{
+        longitude: 127.027711,
+        latitude:37.497997
+      },
+    }
+  }
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition((position)=>{
+      const lat = position.coords.latitude;
+      const long = position.coords.longitude;
+      this.setState({currentCoords:{longitude: long,latitude: lat}})
+    })
+  }
   render() {
+    const {currentCoords,defaultCoords} = this.state
     return (
       <div className="App">
         <Header></Header>
-        <Mgkmap></Mgkmap>
+        <Mgkmap
+          currentCoords={currentCoords}
+          defaultCoords={defaultCoords}
+        ></Mgkmap>
         <Chatlist></Chatlist>
       </div>
     );
--- src/components/Mgkmap.js
+++ src/components/Mgkmap.js
@@ -1,11 +1,51 @@
 import React, {Component} from 'react';
 
 class Mgkmap extends Component {
-  render() {
-    return (
-      <section id="map" class="main">지도</section>
-    );
-  }
+  mapContainer = React.createRef();
+
+  componentDidMount(){
+    const { latitude, longitude } = this.props.defaultCoords;
+    const currentCoords = new window.naver.maps.LatLng(latitude,longitude);
+        this.map = new window.naver.maps.Map(this.mapContainer.current, {
+            center: currentCoords,
+            zoom:11,
+        });
+        this.currentMarker = new window.naver.maps.Marker({
+            position: currentCoords,
+            title:'default 위치',
+            map: this.map,
+            icon:{
+                content: '<div style="width:20px;height:20px;background:red;border-radius:50%"></div>'
+            }
+        })
+    }
+    render() {
+        return (
+            <div
+                class="main"
+                ref={this.mapContainer}
+            >지도</div>
+        );
+    }
+
+    componentDidUpdate(){
+        console.log('componentDidUpdate');
+        const {longitude,latitude} = this.props.currentCoords;
+        const newCurrentCoords = new window.naver.maps.LatLng(latitude,longitude);
+        const {map} = this;
+        if(!!this.currentMarker){
+            this.currentMarker.setMap(null);
+        }
+        this.currentMarker = new window.naver.maps.Marker({
+            position: newCurrentCoords,
+            title:'현위치',
+            map: this.map,
+            icon:{
+                content: '<div style="width:20px;height:20px;background:red;border-radius:50%"></div>'
+            }
+        })
+        map.setCenter(newCurrentCoords);
+    }
 }
 
 export default Mgkmap;
