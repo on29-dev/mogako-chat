256e6ad24c7e572fd88e65b8eb7bf93771bd23fa
girin-dev <kw8799@gmail.com>
2019-07-02 16:35:44 +0900

    [기능추가] App 라우팅으로 /과 /chatlist(Mainapp)으로 분기 react-router-dom으로 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Route,BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 import Mainapp from './components/Mainapp';
@@ -7,10 +8,10 @@ import Mainapp from './components/Mainapp';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Login></Login>
-        <Mainapp></Mainapp>
-      </div>
+      <BrowserRouter>
+        <Route exact path="/" component={Login}/>
+        <Route path="/chatlist" component={Mainapp}/>
+      </BrowserRouter>
     );
   }
 }
