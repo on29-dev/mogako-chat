f1586643487437687301b0cdd6166831dc973564
girin-dev <kw8799@gmail.com>
2019-07-03 17:59:03 +0900

    [기능추가] Chatlist 전달된 채팅방 리스트 데이터에 따라 Chatlist를 렌더하게 함

--- src/components/Chatlist.js
+++ src/components/Chatlist.js
@@ -2,31 +2,38 @@ import React, {Component} from 'react';
 import '../style/mgkmap.css';
 
 class Chatlist extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      chatList:[
+        {id:1,title:'채팅방1',memberNum:5,recentMsg:'최근메시지1최근메시지1최근메시지1최근메시지1최근메시지1최근메시지1최근메시지1',recentTime:'오후 3:15',},
+        {id:2,title:'채팅방2',memberNum:32,recentMsg:'최근메시지2',recentTime:'오후 4:15',},
+        {id:3,title:'채팅방3',memberNum:1,recentMsg:'최근메시지3',recentTime:'오후 12:12',},
+      ]
+    }
+  }
+
+  chatRoom(){
+    let chatlist=[];
+    const chatrooms = Array.from(this.state.chatList);
+    for (let item of chatrooms){
+      chatlist.push(
+        <li key={item.id}>
+          <h2 className="chat-item-title">{item.title}</h2><span className="chat-item-people">{item.memberNum}</span>
+          <div className="chat-item-message">{item.recentMsg}</div>
+          <time className="chat-item-time">{item.recentTime}</time>
+        </li>
+      )
+    }
+    return chatlist;
+  }
+
   render() {
     return (
       <section id="chat" className="chat aside">
         <h1 className="hidden">채팅방리스트</h1>
         <ul id="chatList" className="chat-list">
-            <li>
-                <h2 className="chat-item-title">채팅방1</h2><span className="chat-item-people">5</span>
-                <p className="chat-item-message">최근 메시지 입니다.최근 메시지 입니다.최근 메시지 입니다.최근 메시지 입니다.최근 메시지 입니다.</p>
-                <time className="chat-item-time">오후 3:51</time>
-            </li>
-            <li>
-                <h2 className="chat-item-title">채팅방2</h2><span className="chat-item-people">5</span>
-                <p className="chat-item-message">최근 메시지 입니다.최근 메시지 입니다.최근 메시지 입니다.최근 메시지 입니다.최근 메시지 입니다.</p>
-                <time className="chat-item-time">오후 3:51</time>
-            </li>
-            <li>
-                <h2 className="chat-item-title">채팅방3</h2><span className="chat-item-people">5</span>
-                <p className="chat-item-message">최근 메시지 입니다.최근 메시지 입니다.최근 메시지 입니다.최근 메시지 입니다.최근 메시지 입니다.</p>
-                <time className="chat-item-time">오후 3:51</time>
-            </li>
-            <li>
-                <h2 className="chat-item-title">채팅방4</h2><span className="chat-item-people">5</span>
-                <p className="chat-item-message">최근 메시지 입니다.최근 메시지 입니다.최근 메시지 입니다.최근 메시지 입니다.최근 메시지 입니다.</p>
-                <time className="chat-item-time">오후 3:51</time>
-            </li>
+          {this.chatRoom()}
         </ul>
         <button id="chatHide" className="chat-hide"><span className="hidden">챗팅리스트 숨기기</span></button>
     </section>
