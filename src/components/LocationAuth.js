b37c4b7f1d88fa20ed88f1b35ef939a3ec658082
girin-dev <kw8799@gmail.com>
2019-07-29 01:19:27 +0900

    [기능수정] LocationAuth 위치 인증여부에 따라 노출조건,  안내문구, 버튼문구, close버튼 등 설정

--- src/components/LocationAuth.js
+++ src/components/LocationAuth.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
-import '../style/locationAuth.css'
+import '../style/locationAuth.css';
+import '../style/modal.css';
 
 class LocationAuth extends Component {
   render(){
-    console.log(this.props)
     return (
       <section className="modal-block">
         <div className="location-auth modal-window">
-          {this.props.comments.infoComment}
-          <button
-            onClick={this.props.getCurrentPosition}
-          >{this.props.comments.btnComment}</button>
+        <h2 className="modal-title">현 위치 인증</h2>
+          <div className="modal-content">
+            <div className="modal-content-comment">{this.authComment().infoComment}</div>
+            <button className="btn-submit"
+              onClick={this.getCurrentPosition}
+            >{this.authComment().btnComment}</button>
+          </div>
+            {!!this.props.isLocaAuth
+              ? <i className="fas fa-times btn-modal-close" onClick={this.props.onClose}></i>
+              : null
+            }
         </div>
       </section>
     )
   }
   
+  authComment=_=>{
+    let comments;
+    if(!!this.props.isLocaAuth){
+        comments = {
+            infoComment:`재인증시 기존의 인증되었던 위치의 일부 채팅방에서 나가질 수 있습니다. 그래도 현위치로 재인증하시겠습니까?`, 
+            btnComment: `재인증하기`}
+    } else {
+        comments = {
+            infoComment:`모각코 맵을 사용하시려면 현재 위치를 인증하셔야 합니다. 인증하기 버튼을 눌러 현재 위치의 채팅방의 사람들과 소통하세요!`,
+            btnComment:'인증하기'}
+    }
+    return comments;
+  }
 }
 
 export default LocationAuth;
--- src/components/Mgkmap.js
+++ src/components/Mgkmap.js
@@ -45,11 +45,12 @@ class Mgkmap extends Component {
             >
                 <div className="map-element">
                     <span className="map-address">위치인증: {this.state.currentAddress}</span>
-                    {this.state.modal && (
+                    {(!this.state.isLocaAuth || this.state.modal) && (
                         <ModalPortal>
                             <PrivateModal path="/location-auth" component={LocationAuth}
-                                comments={this.authComment()}
+                                isLocaAuth={this.state.isLocaAuth}
                                 getCurrentPosition={this.getCurrentPosition}
+                                onClose={this.onClose}
                             />
                         </ModalPortal>
                     )}
@@ -62,18 +63,8 @@ class Mgkmap extends Component {
             </div>
         );
     }
-    authComment=_=>{
-        let comments;
-        if(!!this.state.isLocaAuth){
-            comments = {
-                infoComment:`재인증시 기존의 인증되었던 위치의 일부 채팅방에서 나가질 수 있습니다. 그래도 현위치로 재인증하시겠습니까?`, 
-                btnComment: `재인증하기`}
-        } else {
-            comments = {
-                infoComment:`모각코 맵을 사용하시려면 현재 위치를 인증하셔야 합니다. 인증하기 버튼을 눌러 현재 위치의 채팅방의 사람들과 소통하세요!`,
-                btnComment:'인증하기'}
-        }
-        return comments;
+    onClose=_=>{
+        this.setState({modal:!this.state.modal})
     }
     locationAuthToggle=_=>{
         this.setState({modal:!this.state.modal})
